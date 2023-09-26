87a964729ef6a5ca19bcc32966304f5fb2ea3f32
Brenda <amaya.brendaa@gmail.com>
2023-09-25 21:15:16 -0700

    post info is populating in inspect

--- project3/src/Components/PostPage.js
+++ project3/src/Components/PostPage.js
@@ -1,6 +1,21 @@
 import React from "react"
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { useState } from "react"
 
 export default function PostPage(){
+    const {id} = useParams()
+    const [postInfo, setPostInfo] = useState('')
+
+    useEffect(()=> {
+        fetch(`http://localhost:2222/post/${id}`)
+        .then(response => {
+            response.json(postInfo => {
+                setPostInfo(postInfo)
+            })
+        })
+    }, [])
+
     return(
         <div>post page</div>
     )
